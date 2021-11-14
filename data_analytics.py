fdeaa832ef892f1f19c5119bd10178909cde3e89
danielgof <gofmandon@gmail.com>
2021-11-14 07:31:38 +0300

    some upd

--- data_analytics.py
+++ data_analytics.py
@@ -28,16 +28,58 @@ NVOS = pd.read_excel("Data/NVOS.xlsx")
 register_of_licenses = pd.read_excel("Data/register_of_licenses.xlsx")
 reg_pollutant = pd.read_excel("Data/reg_pollutant.xlsx")
 
-print(NVOS.columns.values)
 
-NVOS.groupby('Категория \nобъекта НВОС')['Кол-во \nисточников выбросов'].nunique().reset_index().sort_values('Кол-во \nисточников выбросов', ascending=True)
 
 egrul['Вид деятельности'].unique()
 
 print(egrul.columns.values)
 
-def detector(x_c, y_c, r):
-    pass
+
+
+
+
+place = '454080, г. Челябинск, п. Мелькомбинат 2 участок 1, д. 37'
+location = GoogleV3(api_key="AIzaSyApxlxXIdRM9sWefCgQNHTdCewnxkccb_Q", domain="maps.google.ru").geocode(place)
+print(location.address)
+print(location.latitude, location.longitude)
+
+data_address = list(egrul['Адрес'])
+
+# долгота и широта адреса
+def convert(address):
+    location = GoogleV3(api_key="AIzaSyApxlxXIdRM9sWefCgQNHTdCewnxkccb_Q", domain="maps.google.ru").geocode(address)
+    return [location.latitude, location.longitude]
+
+coord = convert(data_address[1])
+
+print(coord)
+print(type(coord))
+
+# долгота и широта адресов преприятий
+coordinates = list(map(convert, data_address))
+
+# долгота и широта адресов преприятий в килллометрах
+def to_x_and_y(coordinates):
+    inProj = Proj(init='epsg:3857')
+    outProj = Proj(init='epsg:4326')
+    x1, y1 = coordinates[0], coordinates[1]
+    x2, y2 = transform(inProj, outProj, x1 ,y1)
+    return [x2, y2]
+
+
+coor_x_y = list(map(to_x_and_y, coordinates))
+
+
+def in_circle(center_x, center_y, radius, x, y):
+    if square_dist <= radius ** 2:
+
+    return square_dist <= radius ** 2
+
+def detector(x_c, y_c, r, coor_x_y):
+    square_dist = (x_c - x) ** 2 + (y_c - y) ** 2
+
+
+
 
 bad_performances = ['обработка металлов', 'обращение с отходами', '']
 
@@ -48,53 +90,4 @@ def similarity(s1, s2):
     return matcher.ratio()
 
 similarity(df[2], data[0])
-
-
-
-egrul.shape
-
-len(egrul['Адрес'].unique())
-
-place = '454080, г. Челябинск, п. Мелькомбинат 2 участок 1, д. 37'
-location = GoogleV3(api_key="AIzaSyApxlxXIdRM9sWefCgQNHTdCewnxkccb_Q", domain="maps.google.ru").geocode(place)
-print(location.address)
-print(location.latitude, location.longitude)
-
-data_address = list(egrul['Адрес'])
-
-def convert(address):
-    location = GoogleV3(api_key="AIzaSyApxlxXIdRM9sWefCgQNHTdCewnxkccb_Q", domain="maps.google.ru").geocode(address)
-    return [location.latitude, location.longitude]
-
-coord = convert(data_address[1])
-
-print(coord)
-print(type(coord))
-
-coordinates = list(map(convert, data_address))
-
-!pip install pyproj
-
-def to_x_and_y(coordinates):
-    inProj = Proj(init='epsg:3857')
-    outProj = Proj(init='epsg:4326')
-    x1,y1 = -11705274.6374, 4826473.6922
-    x2,y2 = transform(inProj,outProj,x1,y1)
-    return x2,y2
-
-
-
-inProj = Proj(init='epsg:3857')
-outProj = Proj(init='epsg:4326')
-x1,y1 = -11705274.6374, 4826473.6922
-x2,y2 = transform(inProj,outProj,x1,y1)
-print (x2,y2)
-
-def in_circle(center_x, center_y, radius, x, y):
-    square_dist = (center_x - x) ** 2 + (center_y - y) ** 2
-    return square_dist <= radius ** 2
-
-
-
-
 
